a26360faf1c4ab1bc653f17754c4bcbd8f0fb30f
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-07-17 09:19:45 -0700

    implemented all tasks successfully and efficiently

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,9 +12,18 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def __str__(self):
+        current = self.head
+        text = ""
+        while current:
+            text += f"{current.get_value()} "
+            current = current.get_next()
+        return text
 
     def add_to_head(self, value):
         node = Node(value)
@@ -39,4 +48,55 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        # if node is None or node.get_next() is None:
+        #     return node
+        # recursion = self.reverse_list(node.get_next(), prev)
+        # node.next_node.next_node = node
+        # node.next_node = None
+        # return recursion
+
+        # NEW PLAN:
+        # 1. to avoid infinite loop, recursive call should call the NEXT node all the
+        # way down, only breaking the recursion cycle when there is no next, (it has
+        # reached the end of the list)
+        # 2. the next property of each node should be overridden with what used to be
+        # the previous node
+        # 3. recursively call the next node, defining its new previous node as the
+        # current iteration's node parameter
+        # [1, 2, 3, 4, 5].reverse_list(list.head, None)
+        # next = 2
+        # set new next to "none"
+        # then call
+        # self.reverse_list(next, current)
+
+        # UPDATE: error 2 -> this will stop us from ever operating on the "None" constant
+        if not node:
+            return
+
+        # UPDATE for error (oof):
+        # when we hit the last node we also need to update the head <- duh *facepalm*
+        if node.get_next() is None:
+            # 1. this means we're at the last node, which should be the new head!
+            self.head = node
+            node.set_next(prev)
+            return
+
+        # 2. save the next node before update
+        next = node.get_next()
+        # then update the next prop with our current iteration's previous param
+        node.set_next(prev)
+
+        # the current node is going to be the new previous of the next node <--
+        self.reverse_list(next, node)
+
+
+x = LinkedList()
+x.add_to_head(1)
+x.add_to_head(2)
+x.add_to_head(3)
+x.add_to_head(4)
+x.add_to_head(5)
+print(x)
+x.reverse_list(x.head, None)
+print(x)
+# IT FINALLY WORKS
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,13 @@
 class RingBuffer:
     def __init__(self, capacity):
+        self.curIndex = 0
+        self.capacity = capacity
+        self.list = [None] * self.capacity
         pass
 
     def append(self, item):
-        pass
+        self.list[self.curIndex] = item
+        self.curIndex = self.curIndex + 1 if self.curIndex < self.capacity - 1 else 0
 
     def get(self):
-        pass
+        return [x for x in self.list if x]
